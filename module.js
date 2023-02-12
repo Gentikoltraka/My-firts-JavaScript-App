6eccfb241d7f26e48bb9278d0fc321609f6ae498
Gentikoltraka <123014779+Gentikoltraka@users.noreply.github.com>
2023-02-12 23:42:21 +0100

    Add files via upload

---
+++ module.js
@@ -0,0 +1,28 @@
+
+
+let pointNumber=document.getElementById(points)
+console.log(pointNumber)
+
+
+
+
+
+
+
+pointNumber=3
+function Increase() {
+pointNumber += +3
+points.innerText=pointNumber
+
+console.log(pointNumber)
+}
+
+function decrease() {
+    pointNumber += -1
+
+    points.innerText=pointNumber
+    console.log(pointNumber)
+}
+
+
+console.log(10)
